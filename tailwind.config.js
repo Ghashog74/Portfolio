57409a21c16821939965f6a93f2387e444ed1c09
quentin <quentinmontreuil06@orange.fr>
2024-10-15 16:53:36 +0200

    Instalation NodeJS et Tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        'text': 'var(--text)',
+        'background': 'var(--background)',
+        'primary': 'var(--primary)',
+        'secondary': 'var(--secondary)',
+        'accent': 'var(--accent)',
+       },       
+    },
+  },
+  plugins: [],
+}
